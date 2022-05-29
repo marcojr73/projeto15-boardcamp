6ff12864e5131150cbd5cbf022768d8575774a2e
marco junior <marcojuniorengineer@gmail.com>
2022-05-29 20:58:56 -0300

    e possivel fazer a devoluçao de um jogo

--- src/controllers/controllersRentals.js
+++ src/controllers/controllersRentals.js
@@ -1,5 +1,6 @@
 import connectDB from "../config/bank.js"
 import dayjs from "dayjs"
+import { parse } from "pg-protocol"
 
 export async function deleteRental(req, res) {
 
@@ -10,8 +11,9 @@ export async function deleteRental(req, res) {
         const remove = await db.query(`
             DELETE FROM rentals WHERE id=$1;
             `, [id])
-            
+
         res.sendStatus(200)
+        
     } catch (error) {
         res.send(error)
         console.log(error)
@@ -19,7 +21,36 @@ export async function deleteRental(req, res) {
 }
 
 export async function insertRental(req, res) {
+    const returnDate = dayjs().$d
+    // const returnDate = new Date("2022-06-02")
+    let delayFee = null
+    const {id} = req.params
 
+    try {
+        const db = await connectDB()
+        const rental = await db.query(`
+            SELECT * FROM rentals WHERE id=$1;
+        `,[id])
+        const pricePerDay = rental.rows[0].originalPrice/rental.rows[0].daysRented
+        const {rentDate} = rental.rows[0]
+        
+
+        const timeDiff = Math.abs(returnDate.getTime() - rentDate.getTime());
+        const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        const delay = diffDays - rental.rows[0].daysRented
+
+
+        if(delay > 0){
+            delayFee = delay * pricePerDay 
+            const update = db.query(`
+                UPDATE rentals SET "delayFee" = $1, "returnDate" = $2  WHERE id =  $3;
+            `, [delayFee, returnDate, id])
+        }
+        res.sendStatus(200)
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export async function insertRentals(req, res) {
